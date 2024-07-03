4d0c2068138fd75907495a3b73a2bc704028dbcc
judgelord <devin.jl@gmail.com>
2024-07-03 13:13:41 -0400

    add word count and cite Case C

--- code/get_appendix.R
+++ code/get_appendix.R
@@ -4,13 +4,13 @@ googledrive::drive_auth(email = "devin.jl@gmail.com")
 
 googledrive::drive_download(
   "https://docs.google.com/document/d/1CR5b3gDvfipxWcqheFHOOeAX2GMLg_9SJT54ByqqBJo",
-  path = here::here("efficacy_appendix.txt"),
+  path = here::here("assets", "efficacy_appendix.txt"),
   overwrite = TRUE
   )
 
 googledrive::drive_download(
   "https://docs.google.com/spreadsheets/d/1GeO5ndnTqqLoT6DwLkf5l5Z2sRQHlMm5-93aMr3GoZk",
-  path = here::here("efficacy_appendix_table.csv"),
+  path = here::here("assets", "efficacy_appendix_table.csv"),
   overwrite = TRUE
 )
 
